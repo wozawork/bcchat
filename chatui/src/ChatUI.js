3937c6bdb04ac42c9c9526a5eb0a24283eb214cc
muhunthan <muhunthan.nagesh@biocatch.com>
2023-06-06 22:26:57 +1000

    UI changes

--- chatui/src/ChatUI.js
+++ chatui/src/ChatUI.js
@@ -1,82 +1,65 @@
 import React, { useState } from 'react';
 import { Input, Button } from 'antd';
 import { Link } from 'react-router-dom';
-import { Modal } from 'antd';
 import './ChatUI.css';
 
 const ChatUI = () => {
-    //const [messages, setMessages] = useState([]);
 
     const [messages, setMessages] = useState([]);
     const [userInput, setUserInput] = useState('');
     const [sources, setSources] = useState([]);
     const [modalVisible, setModalVisible] = useState(false);
-    const linksArray = ['Link 1', 'Link 2', 'Link 3'];
-    const showModal = () => {
-        setModalVisible(true);
-    };
-
-    const closeModal = () => {
-        setModalVisible(false);
-    };
 
     // Function to handle user messages
     const handleUserMessage = async () => {
-        console.log("called")
-        // if (userInput.trim() === '') return;
 
         const text = userInput.trim();
-        addMessageToChat('user', text);
-        //const response = await sendMessageToAPI(text);
+
+        const textWithheader = (
+            <>
+                <h4>Question:</h4>
+                {text}{' '}
+            </>
+        )
+        addMessageToChat('user', textWithheader);
+
         const response = await queryBCchatbackend(text);
-        //const data = response.choices[0].text;
+
         const data = response.text;
+        const sources = response.sources;
+        setSources(sources);
+
         const dataWithSources = (
             <>
-                {data}{' '}
-                <Button onClick={showModal}>
-                    sources
-                </Button>
+                <h4>Answer:</h4>
+                <div className='chat-answer'>
+                    {data}{' '}
+                </div>
+                <br />
+                <h4>Sources:</h4>
+                <div className="links-container">
+                    {sources.map((link, index) => (
+                        <Link key={index} to={`${link.toLowerCase()}`}>
+                            {link}
+                        </Link>
+                    ))}
+                </div>
             </>
         );
-        const sources = response.sources;
+
 
         const botReply = dataWithSources; // Adjust this based on your API response structure
         const updatedMessages = [
             ...messages,
-            { user: true, content: userInput },
+            { user: true, content: textWithheader },
             { user: false, content: botReply },
         ];
         setMessages(updatedMessages);
-        setSources(sources);
+
         //addMessageToChat('assistant', reply);
         setUserInput('');
     };
 
-
-    const sendMessageToAPI = async (text) => {
-        const payload = {
-            model: 'text-davinci-003',
-            prompt: `User: ${text}\nAssistant:`
-        };
-
-        // Send the payload to the OpenAI API endpoint using your preferred HTTP library
-        const response = await fetch('https://api.openai.com/v1/completions', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer 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' // Replace with your OpenAI API key
-            },
-            body: JSON.stringify(payload)
-        });
-
-        const data = await response.json();
-        return data;
-    };
-    const viewSources = () => {
-        // Your function logic here
-        console.log('Open sources');
-    };
     const queryBCchatbackend = async (text) => {
 
         const payload = {
@@ -98,28 +81,6 @@ const ChatUI = () => {
         setMessages([...messages, newMessage]);
     };
 
-    const handleSendMessage = async () => {
-        const userInput = document.getElementById('user-input').value.trim();
-        if (userInput !== '') {
-            const response = await fetch('YOUR_API_ENDPOINT', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    // Add any other headers required for your API
-                },
-                body: JSON.stringify({ message: userInput }),
-            });
-            const data = await response.json();
-            const botReply = data.reply; // Adjust this based on your API response structure
-            const updatedMessages = [
-                ...messages,
-                { user: true, content: userInput },
-                { user: false, content: botReply },
-            ];
-            setMessages(updatedMessages);
-            //document.getElementById('user-input').value = '';
-        }
-    };
 
     return (
         <div><h2 className="chat-title">BCchat</h2> {/* Title element */}
@@ -139,7 +100,7 @@ const ChatUI = () => {
                 <div className="chat-input">
                     <Input.TextArea
                         id="user-input"
-                        rows={4}
+                        rows={2}
                         value={userInput}
                         onChange={(e) => setUserInput(e.target.value)}
                         onKeyDown={(e) => {
@@ -153,19 +114,11 @@ const ChatUI = () => {
                         Ask
                     </Button>
                 </div>
-                <Modal visible={modalVisible} onCancel={closeModal} footer={null} width={1200} height={900}>
-                    <div className="links-container">
-                        {sources.map((link, index) => (
-                            <Link key={index} to={`${link.toLowerCase()}`}>
-                                {link}
-                            </Link>
-                        ))}
-                    </div>
-
-                </Modal>
             </div>
         </div>
     );
 };
 
 export default ChatUI;
+
+
